44713355eb055d8f762502117cfbacd2e9bc1e28
JuanDavidPena <j_david0915@hotmail.com>
2021-09-30 21:00:24 +0000

    [modify]-pep8

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -6,7 +6,7 @@ import sys
 
 
 def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
+    """Initialize an `n`x`n` sized chessboard with ' '."""
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
