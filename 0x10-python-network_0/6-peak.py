d1755c1e3e5b0ef60e39d095028e9206beb56630
JuanDavidPena <j_david0915@hotmail.com>
2022-01-10 19:20:37 -0500

    [add]-task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+find the peek
+"""
+
+
+def find_peak(arr):
+    """
+        find the peek
+    """
+
+    if arr is None or len(arr) == 0:
+        return None
+    if len(arr) == 1:
+        return max(arr)
+
+    prev = arr[0]
+
+    for i in range (1, len(arr)- 1):
+        if arr[i] > prev and arr[i + 1] < arr[i]:
+            return arr[i]
+        prev = arr[i]
+    return max(arr)
